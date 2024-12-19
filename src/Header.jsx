4928dac88f27830e2022a8276c86408c41b6e276
Mithrildagger <nadimchatellier@gmail.com>
2024-12-19 18:04:38 +0000

    changed the name of the site

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 // Header.js
 function Header() {
-    return <h1 className="header-title">General Kenobi!</h1>;
+    return <h1 className="header-title">Mithril News</h1>;
 }
 
 export default Header;
